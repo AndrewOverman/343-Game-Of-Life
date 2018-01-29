7923fdf6681b5e38928e12bb76c2d74cf5789055
Andrew Overman <overmana@eos11.cis.gvsu.edu>
2018-01-29 14:58:53 -0500

    Added a few headers from the desciption and comments

--- file_utilities.h
+++ file_utilities.h
@@ -1,1 +1,12 @@
-New .h file dont know what happened to the last one
+/*
+ *Header file for Game of Life simulator
+ *Created By: Andrew Overman & Tim VanDyke(?maybe right spelling)
+ *Date: January 29, 2018
+ *
+ */
+
+//Reads in the contents of a file to the program
+int read_file( char* filename, char **buffer );
+
+//Saves the contents of the simulator to a file.
+int write_file( char* filename, char *buffer, int size);
