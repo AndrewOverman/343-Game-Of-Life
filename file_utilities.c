cc167b19b78cba2137cbd3987b115a8db88ae3ad
Andrew Overman <overmana@eos11.cis.gvsu.edu>
2018-02-07 11:47:21 -0500

    added headers and comments

--- file_utilities.c
+++ file_utilities.c
@@ -1,1 +1,20 @@
+/*
+ * Created By: Andrew Overman 
+ * Date: 2/7/2018
+ * C Code for the file utilities methods
+ *
+ */
 
+#include <stdio.h>
+
+int read_file( char* filename, char **buffer ){
+   
+
+
+}
+
+int write_file( char* filename, char *buffer, int size){
+
+
+
+}
