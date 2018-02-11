3823ad7abea28a9c20c20308a16be4e188fada77
Andrew Overman <overmana@eos11.cis.gvsu.edu>
2018-02-10 22:33:24 -0500

    Updated Main

--- Main.c
+++ Main.c
@@ -5,25 +5,45 @@
  *
  */
 
-#include "file_utilities.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+	#include "file_utilities.h"
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <unistd.h>
+
 /*
  *This is a main method to run the whole project
  *@Param int argc: The number of arguments given in the command line
  *@Param char** argv: The array of arguments given in the command line
  */
+
 int main(int argc, char** argv){
 
 	char* buffer = "hello there test";
+	char userinput[80];
+
 	printf("%s\n", argv[1]);
 	printf("%s\n", argv[2]);
 	createBoard(atoi(argv[1]),atoi(argv[2]));
-	write_file("test.txt",buffer,0);
-	read_file("test.txt",argv);
+	copyBoard();
 
+	while(){
+		update();
+	
+		printf("Options: \n");
+		printf("		-Continue\n");
+		printf("		-Save\n");
+		printf("		-Load\n");
+		scanf("%s",userinput);
 
-
+		if(userinput.tolower = "continue"){
+			break;	
+		}
+		else if(userinput.tolower = "save"){
+			write_file(placeholder,placeholder,placerholder);
+		}
+		else{
+			read_file(placeholder,placeholder,placeholder);
+		}
+	}
 }
 
